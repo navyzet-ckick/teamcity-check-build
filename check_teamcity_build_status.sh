1a0cb27e3130d9c45970d9dca03b086ed2a11800
Nikita Lipchik <n.lipchik@clickadu.com>
2019-07-09 13:36:16 +0300

    New fix

--- check_teamcity_build_status.sh
+++ check_teamcity_build_status.sh
@@ -11,6 +11,14 @@ LogFile="/tmp/check_teamcity_build_status.log"
 ######################
 BuildTypeId="${1}"
 
+BuildTypeId="${1}"
+
+if [ $# -eq 0 ]
+  then
+    echo "Run ./check_teamcity_build_status.sh BuildTypeIdTeamcity"
+    exit 2
+fi
+
 mkdir -p $FolderStatus
 
 get_api () {
@@ -21,11 +29,14 @@ get_api () {
 
 slack_send () {
         TEXT=${1}
-        curl --silent --data-urlencode "payload={\"text\": \"${TEXT}\", \"channel\": \"${SlackChanel}\", \"username\": \"${SlackUserName}\", \"as_user\": \"true\", \"link_names\": \"true\", \"icon_emoji\": \":slack:\" }" ${SlackHookUrl}
-        echo ${TEXT}
+        curl --silent --data-urlencode "payload={\"text\": \"${TEXT}\", \"channel\": \"${SlackChanel}\", \"username\": \"${SlackUserName}\", \"as_user\": \"true\", \"link_names\": \"true\", \"icon_emoji\": \":slack:\" }" ${SlackHookUrl} > /dev/null
 }
 
+echo "$(date) Start check Build: ${BuildTypeId}" >> ${LogFile}
+
 Answer=$(get_api "buildTypes/id:${BuildTypeId}/builds/")
+
+echo "Status last build $(echo ${Answer} | jq '.status')"  >> ${LogFile}
 
 case $(echo ${Answer} | jq '.status') in
         '"SUCCESS"')
@@ -37,6 +48,7 @@ case $(echo ${Answer} | jq '.status') in
                         rm "${FolderStatus}/${BuildTypeId}"
                 else
                         echo "File ${FolderStatus}/${BuildTypeId} dont exist. Message dont send"  >> ${LogFile}
+                        echo ":male-mechanic: Success build <$(echo ${Answer} | jq '.webUrl'| sed 's/"//g')&tab=buildLog&state=&expand=all#_state=|${BuildTypeId}>"
                 fi
                 exit 0
                 ;;
@@ -45,10 +57,27 @@ case $(echo ${Answer} | jq '.status') in
                 echo "Build ${BuildTypeId} failed" >> ${LogFile}
                 if [ -f "${FolderStatus}/${BuildTypeId}" ]; then
                         echo "File ${FolderStatus}/${BuildTypeId} exist. Dont send message" >> ${LogFile}
+			echo ":zombie: Failure build <$(echo ${Answer} | jq '.webUrl'| sed 's/"//g')&tab=buildLog&state=&expand=all#_state=|${BuildTypeId}>"
                 else
+			echo "File ${FolderStatus}/${BuildTypeId} dont exist. Send message" >> ${LogFile}
                         slack_send ":zombie: Failure build <$(echo ${Answer} | jq '.webUrl'| sed 's/"//g')&tab=buildLog&state=&expand=all#_state=|${BuildTypeId}>"
+			echo ":zombie: Failure build <$(echo ${Answer} | jq '.webUrl'| sed 's/"//g')&tab=buildLog&state=&expand=all#_state=|${BuildTypeId}>"
                         touch "${FolderStatus}/${BuildTypeId}"
+			echo "Create file ${FolderStatus}/${BuildTypeId}" >> ${LogFile}
                 fi
                 exit 2
+                ;;
+        *)
+                echo "Status last build $(echo ${Answer} | jq '.status')" >> ${LogFile}
+                if [ -f "${FolderStatus}/${BuildTypeId}" ]; then
+                        echo ":zombie: Failure build <$(echo ${Answer} | jq '.webUrl'| sed 's/"//g')&tab=buildLog&state=&expand=all#_state=|${BuildTypeId}>"
+                        echo "Status last build $(echo ${Answer} | jq '.status')" >> ${LogFile}
+                        echo "File ${FolderStatus}/${BuildTypeId} exist. Dont send message" >> ${LogFile}
+                        exit 2
+                else
+                        echo ":male-mechanic: Success build <$(echo ${Answer} | jq '.webUrl'| sed 's/"//g')&tab=buildLog&state=&expand=all#_state=|${BuildTypeId}>"
+			echo "File ${FolderStatus}/${BuildTypeId} dont exist. Dont send message" >> ${LogFile}
+                        exit 0
+                fi
                 ;;
 esac
